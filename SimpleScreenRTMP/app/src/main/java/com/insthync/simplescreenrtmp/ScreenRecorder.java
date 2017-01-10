9140546fa41226914400d43018270001aa680676
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2017-01-10 21:06:03 +0700

    Try to make it work, now I have send video data before start sending video stream data, server can record video but can not watch it live.

--- SimpleScreenRTMP/app/src/main/java/com/insthync/simplescreenrtmp/ScreenRecorder.java
+++ SimpleScreenRTMP/app/src/main/java/com/insthync/simplescreenrtmp/ScreenRecorder.java
@@ -25,13 +25,14 @@ public class ScreenRecorder extends Thread {
     private MediaProjection mMediaProjection;
     // parameters for the encoder
     private static final String MIME_TYPE = "video/avc"; // H.264 Advanced Video Coding
-    private static final int FRAME_RATE = 15; // 15 fps
-    private static final int IFRAME_INTERVAL = 10; // 10 seconds between I-frames
-    private static final int TIMEOUT_US = 1000000000;
+    private static final int FRAME_RATE = 30; // 30 fps
+    private static final int IFRAME_INTERVAL = 5; // 5 seconds between I-frames
+    private static final int TIMEOUT_US = 1000000;  // 1 seconds
     // RTMP_URL Constraints
     private static final String RTMP_URL = "rtmp://192.168.1.45/live/test";
 
     private MediaCodec mEncoder;
+    private MediaFormat mFormat;
     private Surface mSurface;
     private AtomicBoolean mQuit = new AtomicBoolean(false);
     private MediaCodec.BufferInfo mBufferInfo = new MediaCodec.BufferInfo();
@@ -92,7 +93,7 @@ public class ScreenRecorder extends Thread {
             int index = mEncoder.dequeueOutputBuffer(mBufferInfo, TIMEOUT_US);
             Log.i(TAG, "dequeue output buffer index=" + index);
             if (index == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED) {
-
+                Log.d(TAG, "Format changed " + mEncoder.getOutputFormat());
             } else if (index == MediaCodec.INFO_TRY_AGAIN_LATER) {
                 Log.d(TAG, "retrieving buffers time out!");
                 try {
@@ -111,13 +112,30 @@ public class ScreenRecorder extends Thread {
     private void encodeToVideoTrack(int index) {
         ByteBuffer encodedData = mEncoder.getOutputBuffer(index);
 
+        int rtmpConnectionState = mRTMPMuxer != null ? mRTMPMuxer.isConnected() : 0;
+
+        if (startTime == 0)
+            startTime = mBufferInfo.presentationTimeUs / 1000;
+
         if ((mBufferInfo.flags & MediaCodec.BUFFER_FLAG_CODEC_CONFIG) != 0) {
             // The codec config data was pulled out and fed to the muxer when we got
             // the INFO_OUTPUT_FORMAT_CHANGED status.
             // Ignore it.
             Log.d(TAG, "ignoring BUFFER_FLAG_CODEC_CONFIG");
+
+            encodedData.position(0);
+            encodedData.limit(mBufferInfo.size);
+            Log.i(TAG, "sent " + mBufferInfo.size + " bytes to muxer...");
+
+            byte[] bytes = new byte[mBufferInfo.size];
+            encodedData.get(bytes);
+
+            int writeResult = mRTMPMuxer.writeVideo(bytes, 0, bytes.length, 0);
+            Log.d(TAG, "RTMP_URL write format result: " + writeResult);
+
             mBufferInfo.size = 0;
         }
+
         if (mBufferInfo.size == 0) {
             Log.d(TAG, "info.size == 0, drop it.");
             encodedData = null;
@@ -126,6 +144,7 @@ public class ScreenRecorder extends Thread {
                     + ", presentationTimeUs=" + mBufferInfo.presentationTimeUs
                     + ", offset=" + mBufferInfo.offset);
         }
+
         if (encodedData != null) {
             encodedData.position(mBufferInfo.offset);
             encodedData.limit(mBufferInfo.offset + mBufferInfo.size);
@@ -134,35 +153,24 @@ public class ScreenRecorder extends Thread {
             byte[] bytes = new byte[encodedData.remaining()];
             encodedData.get(bytes);
 
-            int rtmpConnectionState = mRTMPMuxer != null ? mRTMPMuxer.isConnected() : 0;
-
-            if (startTime == 0)
-                startTime = mBufferInfo.presentationTimeUs / 1000;
-
-            int timestamp = (int)((mBufferInfo.presentationTimeUs / 1000) - startTime);
-            Log.d(TAG, "RTMP_URL connection state: " + rtmpConnectionState + " timestamp: " + timestamp + " byte[] length: " + bytes.length);
-            if (rtmpConnectionState != 0) {
-                int writeResult = mRTMPMuxer.writeVideo(bytes, 0, bytes.length, timestamp);
-                Log.d(TAG, "RTMP_URL write result: " + writeResult);
-            }
-            else
-            {
-                mRTMPMuxer.open(RTMP_URL, mWidth, mHeight);
-            }
+            int timestamp = (int) ((mBufferInfo.presentationTimeUs / 1000) - startTime);
+            Log.d(TAG, "RTMP connection state: " + rtmpConnectionState + " timestamp: " + timestamp + " byte[] length: " + bytes.length);
+            int writeResult = mRTMPMuxer.writeVideo(bytes, 0, bytes.length, timestamp);
+            Log.d(TAG, "RTMP write data result: " + writeResult);
         }
     }
 
     private void prepareEncoder() throws IOException {
 
-        MediaFormat format = MediaFormat.createVideoFormat(MIME_TYPE, mWidth, mHeight);
-        format.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface);
-        format.setInteger(MediaFormat.KEY_BIT_RATE, mBitRate);
-        format.setInteger(MediaFormat.KEY_FRAME_RATE, FRAME_RATE);
-        format.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, IFRAME_INTERVAL);
+        mFormat = MediaFormat.createVideoFormat(MIME_TYPE, mWidth, mHeight);
+        mFormat.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface);
+        mFormat.setInteger(MediaFormat.KEY_BIT_RATE, mBitRate);
+        mFormat.setInteger(MediaFormat.KEY_FRAME_RATE, FRAME_RATE);
+        mFormat.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, IFRAME_INTERVAL);
 
-        Log.d(TAG, "created video format: " + format);
+        Log.d(TAG, "created video format: " + mFormat);
         mEncoder = MediaCodec.createEncoderByType(MIME_TYPE);
-        mEncoder.configure(format, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
+        mEncoder.configure(mFormat, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
         mSurface = mEncoder.createInputSurface();
         Log.d(TAG, "created input surface: " + mSurface);
         mEncoder.start();
